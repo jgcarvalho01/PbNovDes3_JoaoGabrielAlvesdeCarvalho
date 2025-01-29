f0c968441fc766b56d87753cd3dcea432762d0a4
Joao_Gabriel <jgcarvalho16@outlook.com>
2025-01-29 20:28:44 -0300

    chore: inicializado projeto ms-ticket-management

---
+++ ms-ticket-management/src/main/java/br/com/compass/ms_ticket_management/MsTicketManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.compass.ms_ticket_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTicketManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTicketManagementApplication.class, args);
+	}
+
+}
