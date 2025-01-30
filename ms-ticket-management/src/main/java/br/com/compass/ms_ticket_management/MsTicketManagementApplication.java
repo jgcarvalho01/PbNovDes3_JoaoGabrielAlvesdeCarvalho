fd48d70243130f537ef5f1337ee98691f2ac3ca7
Joao_Gabriel <jgcarvalho16@outlook.com>
2025-01-30 02:23:10 -0300

    feat: adicionado logs no MsTicketManagementApplication

--- ms-ticket-management/src/main/java/br/com/compass/ms_ticket_management/MsTicketManagementApplication.java
+++ ms-ticket-management/src/main/java/br/com/compass/ms_ticket_management/MsTicketManagementApplication.java
@@ -1,15 +1,19 @@
 package br.com.compass.ms_ticket_management;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@Slf4j
 @SpringBootApplication
 @EnableFeignClients(basePackages = "br.com.compass.ms_ticket_management.service")
 public class MsTicketManagementApplication {
 
 	public static void main(String[] args) {
+		log.info("Iniciando o microsserviço Ticket Management...");
 		SpringApplication.run(MsTicketManagementApplication.class, args);
+		log.info("Microsserviço Ticket Management iniciado com sucesso!");
 	}
 
 }
